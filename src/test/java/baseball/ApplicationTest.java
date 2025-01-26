0ed6f97fe43eedbcb021909142c29e3f7ec0aa8d
wldy4627 <wldy4627@gmail.com>
2025-01-26 22:52:09 +0900

    feat: 야구 게임 결과 검증 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,19 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+import baseball.service.BaseballService;
+import baseball.view.Input;
+import baseball.view.Output;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest extends NsTest {
 
@@ -19,6 +26,58 @@ class ApplicationTest extends NsTest {
                 },
                 1, 3, 5, 5, 8, 9
         );
+    }
+
+    @Test
+    void 스트라이크_테스트() {
+        List<Integer> targetNumList = List.of(1, 2, 3);
+        List<Integer> pickedNumList = List.of(1, 5, 6);
+
+        BaseballService baseballService = new BaseballService();
+
+        Result result = baseballService.getNumberResult(targetNumList, pickedNumList);
+
+        assertEquals(1, result.getStrike());
+        assertEquals(0, result.getBall());
+    }
+
+    @Test
+    void 볼_테스트() {
+        List<Integer> targetNumList = List.of(1, 2, 3);
+        List<Integer> pickedNumList = List.of(3, 1, 6);
+
+        BaseballService baseballService = new BaseballService();
+
+        Result result = baseballService.getNumberResult(targetNumList, pickedNumList);
+
+        assertEquals(0, result.getStrike());
+        assertEquals(2, result.getBall());
+    }
+
+    @Test
+    void 스크라이크_볼_테스트() {
+        List<Integer> targetNumList = List.of(1, 2, 3);
+        List<Integer> pickedNumList = List.of(1, 3, 2);
+
+        BaseballService baseballService = new BaseballService();
+
+        Result result = baseballService.getNumberResult(targetNumList, pickedNumList);
+
+        assertEquals(1, result.getStrike());
+        assertEquals(2, result.getBall());
+    }
+
+    @Test
+    void 낫싱_테스트() {
+        List<Integer> targetNumList = List.of(1, 2, 3);
+        List<Integer> pickedNumList = List.of(5, 7, 6);
+
+        BaseballService baseballService = new BaseballService();
+
+        Result result = baseballService.getNumberResult(targetNumList, pickedNumList);
+
+        assertEquals(0, result.getStrike());
+        assertEquals(0, result.getBall());
     }
 
     @Test
